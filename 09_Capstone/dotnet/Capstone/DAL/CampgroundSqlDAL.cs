7769ce30d5cbe18f25a9120127994910204b3bb1
patrickreddy <patrickreddy87@gmail.com>
2019-02-21 13:28:08 -0500

    help

--- 09_Capstone/dotnet/Capstone/DAL/CampgroundSqlDAL.cs
+++ 09_Capstone/dotnet/Capstone/DAL/CampgroundSqlDAL.cs
@@ -1,10 +1,131 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NatParkCampRes.Models;
+using System.Data.SqlClient;
 
 namespace NatParkCampRes.DAL
 {
-    class CampgroundSqlDAL
+    public class CampgroundSqlDAL
     {
+        #region Constants
+        private const string SqlSelectAllCampgrounds = "SELECT * FROM campground;";
+        private const string SqlSelectCampgroundsInPark = "Select * FROM campground where campground.park_id = @ParkId;";
+        #endregion
+
+        #region Member Variables
+        //private const string _getLastIdSQL = "SELECT CAST(SCOPE_IDENTITY() as int);";
+        private string _connectionString;
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="connectionString"></param>
+        public CampgroundSqlDAL(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IList<Campground> GetALLCampgrounds()
+        {
+            List<Campground> output = new List<Campground>();
+
+            //Always wrap connection to a database in a try-catch block
+            try
+            {
+                //Create a SqlConnection to our database
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand(SqlSelectAllCampgrounds, connection);
+
+                    // Execute the query to the database
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    // The results come back as a SqlDataReader. Loop through each of the rows
+                    // and add to the output list
+                    while (reader.Read())
+                    {
+                        // Read in the value from the reader
+                        // Reference by index or by column_name
+                        Campground campground = new Campground();
+                        campground.CampgroundId = Convert.ToInt32(reader["park_id"]);
+                        campground.ParkId = Convert.ToInt32(reader["park_id"]);
+                        campground.Name = Convert.ToString(reader["name"]);
+                        campground.ParkId = Convert.ToInt32(reader["park_id"]);
+                        campground.OpenFromMonth = Convert.ToInt32(reader["open_from_mm"]);
+                        campground.OpenToMonth = Convert.ToInt32(reader["open_to_mm"]);
+                        campground.DailyFee = Convert.ToDecimal(reader["daily_fee"]);
+
+
+                        // Add the department to the output list                       
+                        output.Add(campground);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                // A SQL Exception Occurred. Log and throw to our application!!
+                throw;
+            }
+            return output;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IList<Campground> GetCampgroundsInPark()
+        {
+            List<Campground> output = new List<Campground>();
+
+            //Always wrap connection to a database in a try-catch block
+            try
+            {
+                //Create a SqlConnection to our database
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand(SqlSelectCampgroundsInPark, connection);
+
+                    // Execute the query to the database
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    // The results come back as a SqlDataReader. Loop through each of the rows
+                    // and add to the output list
+                    while (reader.Read())
+                    {
+                        // Read in the value from the reader
+                        // Reference by index or by column_name
+                        Campground campground = new Campground();
+                        campground.CampgroundId = Convert.ToInt32(reader["park_id"]);
+                        campground.ParkId = Convert.ToInt32(reader["park_id"]);
+                        campground.Name = Convert.ToString(reader["name"]);
+                        campground.ParkId = Convert.ToInt32(reader["park_id"]);
+                        campground.OpenFromMonth = Convert.ToInt32(reader["open_from_mm"]);
+                        campground.OpenToMonth = Convert.ToInt32(reader["open_to_mm"]);
+                        campground.DailyFee = Convert.ToDecimal(reader["daily_fee"]);
+
+
+                        // Add the department to the output list                       
+                        output.Add(campground);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                // A SQL Exception Occurred. Log and throw to our application!!
+                throw;
+            }
+            return output;
+        }
+        #endregion
     }
 }
--- 09_Capstone/dotnet/Capstone/DAL/ReservationSqlDAL.cs
+++ 09_Capstone/dotnet/Capstone/DAL/ReservationSqlDAL.cs
@@ -1,10 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NatParkCampRes.Models;
+using System.Data.SqlClient;
 
 namespace NatParkCampRes.DAL
 {
     public class ReservationSqlDAL
     {
+        #region Constants
+        private const string SqlSelectAllReservations = "SELECT * FROM reservation;";
+        #endregion
+
+        #region Member Variables
+        //private const string _getLastIdSQL = "SELECT CAST(SCOPE_IDENTITY() as int);";
+        private string _connectionString;
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="connectionString"></param>
+        public ReservationSqlDAL(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IList<Reservation> GetAllReservations()
+        {
+            List<Reservation> output = new List<Reservation>();
+
+            //Always wrap connection to a database in a try-catch block
+            try
+            {
+                //Create a SqlConnection to our database
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand(SqlSelectAllReservations, connection);
+
+                    // Execute the query to the database
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    // The results come back as a SqlDataReader. Loop through each of the rows
+                    // and add to the output list
+                    while (reader.Read())
+                    {
+                        // Read in the value from the reader
+                        // Reference by index or by column_name
+                        Reservation reservation = new Reservation();
+                        reservation.ReservationId = Convert.ToInt32(reader["reservation_id"]);
+                        reservation.SiteId = Convert.ToInt32(reader["site_id"]);
+                        reservation.Name = Convert.ToString(reader["name"]);
+                        reservation.FromDate = Convert.ToDateTime(reader["from_date"]);
+                        reservation.ToDate = Convert.ToDateTime(reader["to_date"]);
+                        reservation.CreateDate = Convert.ToDateTime(reader["create_date"]);
+
+                        // Add the department to the output list                       
+                        output.Add(reservation);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                // A SQL Exception Occurred. Log and throw to our application!!
+                throw;
+            }
+            return output;
+        }
+        #endregion
     }
 }
